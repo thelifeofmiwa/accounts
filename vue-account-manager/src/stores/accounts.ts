73cab712425b439911679d92d7341628e12fe6b0
Mikhail Alexandrov <mikesperanskii@gmail.com>
2025-02-26 21:23:03 +0300

    Настроена логика по работе с localStorage

--- vue-account-manager/src/stores/accounts.ts
+++ vue-account-manager/src/stores/accounts.ts
@@ -8,21 +8,33 @@ interface Account {
   password: string | null;
 }
 
+const loadAccountsFromLocalStorage = (): Account[] => {
+  const accounts = localStorage.getItem('accounts');
+  return accounts ? JSON.parse(accounts) : [];
+};
+
+const saveAccountsToLocalStorage = (accounts: Account[]) => {
+  localStorage.setItem('accounts', JSON.stringify(accounts));
+};
+
 export const useAccountStore = defineStore('accountStore', {
   state: () => ({
-    accounts: [] as Account[],
+    accounts: loadAccountsFromLocalStorage(),
   }),
   actions: {
     addAccount(account: Account) {
       this.accounts.push(account);
+      saveAccountsToLocalStorage(this.accounts);
     },
     removeAccount(id: number) {
       this.accounts = this.accounts.filter(acc => acc.id !== id);
+      saveAccountsToLocalStorage(this.accounts);
     },
     updateAccount(updatedAccount: Account) {
       const index = this.accounts.findIndex(acc => acc.id === updatedAccount.id);
       if (index !== -1) {
         this.accounts[index] = updatedAccount;
+        saveAccountsToLocalStorage(this.accounts); // Сохраняем в localStorage
       }
     },
   },
